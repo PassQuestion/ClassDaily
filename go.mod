4b96b0a9777153f771c6108826ebd4b0fdbdf80f
PassQuestion <99644257+PassQuestion@users.noreply.github.com>
2024-10-06 11:58:26 +0800

    Add files via upload
    首次上传，不做版本

---
+++ go.mod
@@ -0,0 +1,11 @@
+module Project
+
+go 1.23.2
+
+require (
+	github.com/beevik/etree v1.4.1 // indirect
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
---
+++ windows.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"bytes"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/beevik/etree"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+const (
+	WIDTH  = 320
+	HEIGHT = 700
+)
+
+type hwnd struct {
+	*walk.MainWindow
+}
+
+func get_rand(max int) int64 {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	random_number := rand.Intn(max)
+	for random_number == 0 {
+		random_number = rand.Intn(max)
+	}
+	return int64(random_number)
+}
+func get_date() int {
+	current := time.Now()
+	date := current.Weekday()
+	return int(date)
+}
+func random_window() {
+	type TE struct {
+		*walk.TextEdit
+	}
+	var students_number_line *walk.LineEdit
+	students_number := LineEdit{
+		AssignTo:  &students_number_line,
+		Text:      Bind("请输入学生总数"),
+		TextColor: walk.Color(walk.RGB(100, 100, 100)),
+		MaxLength: 5,
+	}
+	var rand int64
+	var resultpointer *walk.TextEdit
+	randombutton := PushButton{
+		Text: "生成",
+		OnClicked: func() {
+			maxstudent, err := strconv.Atoi(students_number_line.Text())
+			if err == nil {
+				rand = get_rand(maxstudent)
+				result := strconv.FormatInt(rand, 10)
+				resultpointer.SetText(result)
+			}
+		},
+	}
+
+	resulttext := TextEdit{
+		AssignTo: &resultpointer,
+	}
+	widget2 := []Widget{
+		students_number, randombutton, resulttext,
+	}
+	window2pointer, e := walk.NewMainWindow()
+	if e == nil {
+		MainWindow{
+			AssignTo: &window2pointer,
+			Title:    "随机数",
+			Layout:   VBox{},
+			Children: widget2,
+		}.Run()
+	}
+
+}
+func get_class(date int, class int) string {
+	classlist := etree.NewDocument()
+	if err := classlist.ReadFromFile("class.xml"); err == nil {
+		walk.MsgBox(walk.App().ActiveForm(), "无法加载课表", "课表加载失败。请保证课表文件（class.xml）加载正确。", walk.MsgBoxOK)
+	}
+	rootelement := classlist.SelectElement("Days")
+	var dayelement *etree.Element
+	switch date {
+	case int(time.Sunday):
+		return "今日无课"
+	case int(time.Monday):
+		dayelement = rootelement.FindElement("./Day[@id='d1']")
+	case int(time.Tuesday):
+		dayelement = rootelement.FindElement("./Day[@id='d2']")
+	case int(time.Wednesday):
+		dayelement = rootelement.FindElement("./Day[@id='d3']")
+	case int(time.Thursday):
+		dayelement = rootelement.FindElement("./Day[@id='d4']")
+	case int(time.Friday):
+		dayelement = rootelement.FindElement("./Day[@id='d5']")
+	case int(time.Saturday):
+		return "今日无课"
+
+	}
+	var classelement *etree.Element
+	switch class {
+	case 1:
+		classelement = dayelement.FindElement("./Class[@id='l1']name")
+	case 2:
+		classelement = dayelement.FindElement("./Class[@id='l2']name")
+	case 3:
+		classelement = dayelement.FindElement("./Class[@id='l3']name")
+	case 4:
+		classelement = dayelement.FindElement("./Class[@id='l4']/name")
+	case 5:
+		classelement = dayelement.FindElement("./Class[@id='l5']/name")
+	case 6:
+		classelement = dayelement.FindElement("./Class[@id='l6']/name")
+	case 7:
+		classelement = dayelement.FindElement("./Class[@id='l7']/name")
+	case 8:
+		classelement = dayelement.FindElement("./Class[@id='l8']/name")
+	case 9:
+		classelement = dayelement.FindElement("./Class[@id='l9']/name")
+
+	}
+	return classelement.Text()
+
+}
+
+func date_to_string(get_date int) string {
+	if get_date == int(time.Sunday) {
+		return "日"
+	} else if get_date == int(time.Monday) {
+		return "一"
+	} else if get_date == int(time.Tuesday) {
+		return "二"
+	} else if get_date == int(time.Wednesday) {
+		return "三"
+	} else if get_date == int(time.Thursday) {
+		return "四"
+	} else if get_date == int(time.Friday) {
+		return "五"
+	} else if get_date == int(time.Saturday) {
+		return "六"
+	}
+	return "时间错误"
+}
+func main() {
+	str1 := "今天星期"
+	str2 := date_to_string(get_date())
+	var stringBuilder bytes.Buffer
+	stringBuilder.WriteString(str1)
+	stringBuilder.WriteString(str2)
+	rootwindow := new(hwnd)
+	a := stringBuilder.String()
+	date := Label{
+		Text: a,
+	}
+	label1 := Label{
+		Text:          get_class(get_date(), 1),
+		TextAlignment: AlignCenter,
+	}
+
+	label2 := Label{
+		Text:          get_class(get_date(), 2),
+		TextAlignment: AlignCenter,
+	}
+
+	label3 := Label{
+		Text:          get_class(get_date(), 3),
+		TextAlignment: AlignCenter,
+	}
+
+	label4 := Label{
+		Text:          get_class(get_date(), 4),
+		TextAlignment: AlignCenter,
+	}
+
+	label5 := Label{
+		Text:          get_class(get_date(), 5),
+		TextAlignment: AlignCenter,
+	}
+
+	label6 := Label{
+		Text:          get_class(get_date(), 6),
+		TextAlignment: AlignCenter,
+	}
+
+	label7 := Label{
+		Text:          get_class(get_date(), 7),
+		TextAlignment: AlignCenter,
+	}
+
+	label8 := Label{
+		Text:          get_class(get_date(), 8),
+		TextAlignment: AlignCenter,
+	}
+
+	label9 := Label{
+		Text:          get_class(get_date(), 9),
+		TextAlignment: AlignCenter,
+	}
+
+	Settingbutton := PushButton{
+		Text: "设置",
+		OnClicked: func() {
+			walk.MsgBox(rootwindow, "未开发-设置", "以后会将此区域改为课表修改区", walk.MsgBoxOK)
+		},
+	}
+	Randombutton := PushButton{
+		Text:      "随机数",
+		OnClicked: func() { random_window() },
+	}
+	widget := []Widget{
+		date, label1, label2, label3, label4, label5, label6, label7, label8, label9, Settingbutton, Randombutton,
+	}
+	wd1 := MainWindow{
+		AssignTo: &rootwindow.MainWindow,
+		Title:    "电子值日生",
+		MaxSize:  Size{WIDTH, HEIGHT},
+		MinSize:  Size{WIDTH, HEIGHT},
+		Layout:   VBox{},
+		Children: widget,
+	}
+
+	wd1.Run()
+}
